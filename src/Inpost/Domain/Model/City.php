f182ecb43043b985aae34ab28b325d09f3706028
rbrauner <rafalbrauner@gmail.com>
2024-07-16 19:43:34 +0200

    add model

---
+++ src/Inpost/Domain/Model/City.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Inpost\Domain\Model;
+
+final class City
+{
+    public function __construct(
+        private string $name,
+        private AddressDetails $addressDetails
+    ) {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getAddressDetails(): AddressDetails
+    {
+        return $this->addressDetails;
+    }
+
+    public function setAddressDetails(AddressDetails $addressDetails): self
+    {
+        $this->addressDetails = $addressDetails;
+        return $this;
+    }
+}
---
+++ src/Inpost/Domain/Model/InpostResult.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Inpost\Domain\Model;
+
+final class InpostResult
+{
+    /**
+     * @param City[] $items
+     */
+    public function __construct(
+        private int $count = 0,
+        private int $page = 0,
+        private int $totalPages = 0,
+        private array $items = []
+    ) {
+    }
+
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): self
+    {
+        $this->count = $count;
+        return $this;
+    }
+
+    public function getPage(): int
+    {
+        return $this->page;
+    }
+
+    public function setPage(int $page): self
+    {
+        $this->page = $page;
+        return $this;
+    }
+
+    public function getTotalPages(): int
+    {
+        return $this->totalPages;
+    }
+
+    public function setTotalPages(int $totalPages): self
+    {
+        $this->totalPages = $totalPages;
+        return $this;
+    }
+
+    /**
+     * @return list<City>
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param list<City> $items
+     */
+    public function setItems(array $items): self
+    {
+        $this->items = $items;
+        return $this;
+    }
+}
