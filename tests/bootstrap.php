5ffa41bffd3a31f4e75c9bc2bf3c40703459c5e4
rbrauner <rafalbrauner@gmail.com>
2024-07-16 19:32:42 +0200

    prepare tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -10,4 +11,7 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
     require dirname(__DIR__) . '/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test');
 }
+
+set_exception_handler([new ErrorHandler(), 'handleException']);
