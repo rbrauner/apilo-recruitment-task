c7eee781067fbbd8ba645ce5fdd17053d464d179
rbrauner <rafalbrauner@gmail.com>
2024-07-16 20:17:44 +0200

    add cli command

---
+++ src/Inpost/Presentation/Command/GetParcelsForCityCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Inpost\Presentation\Command;
+
+use Override;
+use App\Inpost\Application\Query\GetParcelsForCityQuery;
+use App\Inpost\Domain\Model\InpostResult;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Throwable;
+
+#[AsCommand(
+    name: 'app:inpost:get-parcels-for-city',
+    description: 'Get inpost parcels for city',
+)]
+final class GetParcelsForCityCommand extends Command
+{
+    use HandleTrait;
+
+    public function __construct(
+        private readonly SerializerInterface $serializer,
+        MessageBusInterface $messageBus,
+    ) {
+        $this->messageBus = $messageBus;
+        parent::__construct();
+    }
+
+    #[Override]
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('city', InputArgument::REQUIRED, 'City where parcels are located')
+        ;
+    }
+
+    #[Override]
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        /** @var string $city */
+        $city = $input->getArgument('city');
+
+        try {
+            /** @var InpostResult */
+            $result = $this->handle(new GetParcelsForCityQuery($city));
+        } catch (Throwable $throwable) {
+            $io->error($throwable->getMessage());
+
+            return Command::FAILURE;
+        }
+
+        $serializedResult = $this->serializer->serialize($result, 'json');
+        $io->success(sprintf('Parcels for city %s fetched successfully: %s', $city, $serializedResult));
+
+        return Command::SUCCESS;
+    }
+}
